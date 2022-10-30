bcaceeaed9249bdfe4095d20f4a13a59dea615b5
Bhupati Gudla <bhupatigudla00@gmail.com>
2022-10-30 22:35:18 +0530

    my first connection with git

---
+++ src/test/java/com/spring/learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
